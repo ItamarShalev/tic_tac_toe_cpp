cf1aa077445e11e90d592ced3cb2b3f560965f65
Itamar Shalev <itamar1111111@gmail.com>
2025-01-14 19:53:11 +0200

    project: Run simple game in main
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+#include "piece.h"
+#include "game.h"
+
+using std::cout, std::endl;
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    /*
+     * [0, 1, 2]
+     * [3, 4, 5]
+     * [6, 7, 8]
+     */
+    Game game(3, 3, Piece::X);
+    game.play(0);
+    game.play(3);
+    game.play(1);
+    game.play(4);
+    game.play(2);
+    cout << "Game over: " << std::boolalpha << game.game_over() << endl;
+    cout << "Winner: " << PieceUtils::to_char(game.get_board().get_winner()) << endl;
     return 0;
 }
